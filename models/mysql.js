f53b0efb9e72358bd02f31d10494d34adc724ecb
polluxx <yuriy.kosakivsky@gmail.com>
2015-08-06 11:21:16 +0300

    add fix

--- models/mysql.js
+++ models/mysql.js
@@ -15,15 +15,19 @@ var mysql = require('mysql'),
       },
       proxies: function(params) {
         var self = this, limit = params.limit || 10, page = limit * ((params.page || 1)-1),
-            status = params.status !== undefined ? params.status : 1;
+            status = params.status !== undefined ? params.status : 1, query;
         if(limit > 100) limit = 100;
         return new Promise(function(resolve, decline) {
               proxiesData = self.connect();
 
               proxiesData.then(function(connection) {
+                  query = 'SELECT * FROM seo_proxy where proxy_status = '+ status +' limit '+page+','+limit;
+                  if(params.count !== undefined) {
+                      query = 'SELECT count(*) FROM seo_proxy where proxy_status = '+ status;
+                  }
 
                   connection.query(
-                      'SELECT * FROM seo_proxy where proxy_status = '+ status +' limit '+page+','+limit,
+                      query,
                       function(err, row) {
                           if(err) decline(err);
 
